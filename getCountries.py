c7fc2ce8902871a82797a9c87471995d88e95c5f
Marliess <liezq@live.nl>
2015-02-11 09:56:09 +0100

    Delete getCountries.py

--- getCountries.py
+++
@@ -1,7 +0,0 @@
-def getCountries():
-    countryList = []
-    infile = open("countries_list.txt","r")
-    for line in infile:
-        country = line.split()
-        countryList.append(Country(country))
-        return countryList
