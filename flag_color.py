0ac59519d5bd3e30bea0ac0bc0f8a4cab24e6a3f
Dr1es <richard_neyhoff@hotmail.com>
2015-02-16 16:04:43 +0100

    Poging tot willekeurige kleur maken

--- flag_color.py
+++ flag_color.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
-from PyQt4 import QtGui,QtCore
+from PyQt4 import QtGui, QtCore
 import sys
+from random import randrange
 
 class FlagColor(QtGui.QColor):
     """Constructor"""
     def __init__(self):
         QtGui.QColor.__init__(self)
 
+    def color_picker(self):
 
-
-
-
-
-
+        self.col = QtGui.QtColor(0, 0, 0)
+        val = randrange(255)
+        self.col.setRed(val)
+        self.col.setGreen(val)
+        self.col.setBlue(val)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
