46b843447571ff54054dd2b9e7acd6e49a67d640
Marliess <liezq@live.nl>
2015-02-16 12:57:00 +0100

    To print country objects

--- country.py
+++ country.py
@@ -6,16 +6,11 @@ class Country():
     def __init__(self):
         countries = self.getCountries()
 
-    """def __str__(self):
-        for line in f:
-            return("Hello from {}".format(self.getCountries))"""
-
     def getCountries(self):
         with open("countries_list.txt", "r")as f:
             countries = [line.strip() for line in f]
         for word in countries:
             print("Hello from {}".format(word))
-
 
 def main():
     name = Country()
