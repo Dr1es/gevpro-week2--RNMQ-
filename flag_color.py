3d378f5b1a09cf9efeb81197b5b50409b2aa7416
Dr1es <richard_neyhoff@hotmail.com>
2015-02-18 09:56:10 +0100

    Nieuw vensterbestand en andere wijzigingen

--- flag_color.py
+++ flag_color.py
@@ -2,7 +2,6 @@
 
 from PyQt4 import QtGui, QtCore
 import sys
-from random import randrange
 
 class FlagColor(QtGui.QColor):
     """Constructor"""
@@ -12,13 +11,14 @@ class FlagColor(QtGui.QColor):
     def color_picker(self):
 
         self.col = QtGui.QtColor(0, 0, 0)
-        val = randrange(255)
-        self.col.setRed(val)
-        self.col.setGreen(val)
-        self.col.setBlue(val)
+        self.col.setRed(100)
+        self.col.setGreen(50)
+        self.col.setBlue(25)
+        
+        self.square.setStyleSheet("QFrame { background-color: %s }" % FlagColor.name())
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     flagcolor = FlagColor()
-    flagcolor.show()
+#    flagcolor.show()
     app.exec_()
