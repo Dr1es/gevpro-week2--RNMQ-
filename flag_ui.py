c062c6a2172264909041380f79273caa93d11a62
Dr1es <richard_neyhoff@hotmail.com>
2015-02-18 10:31:06 +0100

    gitignore en venster + country samengevoegd

--- flag_ui.py
+++ flag_ui.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env/python
 
 from PyQt4 import QtGui, QtCore
+from flag_color import FlagColor
 import sys
 
 class FlagUI(QtGui.QWidget):
 	def __init__(self):
 		QtGui.QWidget.__init__(self)
 		self.setWindowTitle("Flags")
+		self.get_countries()
+		
+	def get_countries(self):
+		with open("countries_list.txt", "r")as f:
+			countries = [line.strip() for line in f]
+		for word in countries:
+			print("Hello from {}".format(word))
+			
+		#self.countries = QtGui.QCombobox()
+		#self.countries.addItems(countries)
 	
 	def color_picker(self):
 		
@@ -16,6 +27,7 @@ class FlagUI(QtGui.QWidget):
 		self.col.setBlue(25)
 		
 		self.square.setStyleSheet("QFrame { background-color: %s }" % FlagColor.name())
+	
 	
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
