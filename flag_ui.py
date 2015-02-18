def8eeed344a011d8cc4c895da0fefd7bb8181b0
Marliess <liezq@live.nl>
2015-02-18 10:36:08 +0100

    Vlaggen tekenen

--- flag_ui.py
+++ flag_ui.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env/python
 
 from PyQt4 import QtGui, QtCore
+from flag_color import FlagColor
 import sys
 
 class FlagUI(QtGui.QWidget):
-	def __init__(self):
-		QtGui.QWidget.__init__(self)
-		self.setWindowTitle("Flags")
-	
-	def color_picker(self):
-		
-		self.col = QtGui.QtColor(0, 0, 0)
-		self.col.setRed(100)
-		self.col.setGreen(50)
-		self.col.setBlue(25)
-		
-		self.square.setStyleSheet("QFrame { background-color: %s }" % FlagColor.name())
-	
+        def __init__(self):
+                QtGui.QWidget.__init__(self)
+                self.setWindowTitle("Flags")
+                countries = self.getCountries()
+
+        def getCountries(self):
+                with open("countries_list.txt", "r")as f:
+                        countries = [line.strip() for line in f]
+                for word in countries:
+                        print("Hello from {}".format(word))
+  
+        def color_picker(self):
+                self.col = QtGui.QtColor(0, 0, 0)
+                self.col.setRed(100)
+                self.col.setGreen(50)
+                self.col.setBlue(25)
+    
+                self.square.setStyleSheet("QFrame { background-color: %s }" % FlagColor.name())
+  
 if __name__ == "__main__":
-	app = QtGui.QApplication(sys.argv)
-	flags = FlagUI()
-	flags.show()
-	app.exec_()
+  app = QtGui.QApplication(sys.argv)
+  flags = FlagUI()
+  flags.show()
+  app.exec_()
+
