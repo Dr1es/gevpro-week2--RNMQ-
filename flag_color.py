27dc0af1a2a5bf4371d7a7d3d5485c9daa191734
Marliess <liezq@live.nl>
2015-02-23 12:21:37 +0100

    Give random values to red, green and blue

--- flag_color.py
+++ flag_color.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 from PyQt4 import QtGui, QtCore
+import random
 import sys
 
 class FlagColor(QtGui.QColor):
     """Constructor"""
     def __init__(self):
         QtGui.QColor.__init__(self)
-
+        self.color_picker()
 
     def color_picker(self):
+        """Give a random value to setRed, setGreen and setBlue"""
         flagCol = QtGui.QColor(0, 0, 0)
         color = random.randint(1,255)
         colour = random.randint(1,255)
