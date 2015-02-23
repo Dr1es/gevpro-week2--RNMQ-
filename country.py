c8ff42b84abb782c6b3ad6eecf0ee7a9a6412318
Marliess <liezq@live.nl>
2015-02-23 12:23:13 +0100

    Print countries

--- country.py
+++ country.py
@@ -4,10 +4,13 @@ import sys
 from flag_color import FlagColor
 
 class Country(FlagColor):
+    """Constructor"""
     def __init__(self):
         countries = self.getCountries()
 
     def getCountries(self):
+        """Get data from a text file and print it like
+            Hello from {data}"""
         with open("countries_list.txt", "r")as f:
             countries = [line.strip() for line in f]
         for word in countries:
